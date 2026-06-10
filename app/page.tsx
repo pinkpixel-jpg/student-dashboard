61191a7cf7de4a600175c8d1c2901e417f488d36
pinkpixel-jpg <rashmirahangdale845@gmail.com>
2026-06-10 20:44:58 +0530

    merged dashboard with supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,34 @@
+import Sidebar from "../components/Sidebar";
+import HeroTile from "../components/HeroTile";
+import ActivityTile from "../components/ActivityTile";
+import CourseCard from "../components/CourseCard";
 import { getCourses } from "../lib/getCourse";
 
 export default async function Home() {
   const courses = await getCourses();
 
   return (
-    <main className="min-h-screen bg-black p-8 text-white">
-      <pre>
-        {JSON.stringify(courses, null, 2)}
-      </pre>
+    <main className="flex min-h-screen bg-black">
+      <Sidebar />
+
+      <section className="flex-1 p-6">
+        <div className="grid gap-6 lg:grid-cols-3">
+          <div className="lg:col-span-3">
+            <HeroTile />
+          </div>
+
+          {courses.map((course) => (
+            <CourseCard
+              key={course.id}
+              course={course}
+            />
+          ))}
+
+          <div className="lg:col-span-3">
+            <ActivityTile />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
